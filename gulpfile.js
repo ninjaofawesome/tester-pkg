f057bc879ff4b929e50f471886717f00084fa48a
Hannah Nordgren <hey@hereshannahs.info>
2017-08-11 17:13:52 -0400

    [FEATURE] added mixins, variable and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+  return gulp.src('./scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+});
+
+gulp.task('watch', ['sass'], function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+
